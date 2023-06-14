02b6484b9454f8ca61efa707a0e2156d7e810ae0
Mohamed Baltu <mohamed@cipher.ly>
2023-06-14 19:18:31 +0200

    add swagger and healthcheck

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenArms API')
+    .setDescription('Cipher - OpenArms API description')
+    .setVersion('1.0')
+    .addTag('opnarms')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
